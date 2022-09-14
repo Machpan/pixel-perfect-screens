0d2b2295d6ecc93c3515802cf324fe04a892cb3e
Machpan <machetik@gmail.com>
2022-09-14 12:12:55 +0300

    begining project

---
+++ fheory-financial/fheory-financial/ViewControllers/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  fheory-financial
+//
+//  Created by Владимир Осипов on 14.09.2022.
+//
+
+import UIKit
+
+class LogInController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
